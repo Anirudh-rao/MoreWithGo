16878d2e8f6a82815fbfa8226efc23943ee41d61
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2026-02-27 20:59:02 +0530

    Static Server

---
+++ SimpleWebServer/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request Successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name: %s\n", name)
+	fmt.Fprintf(w, "Address: %s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not found", http.StatusNotFound)
+	}
+	if r.URL.Path != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "hello")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting Server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
